261dff85cb6d8a0635d8e3daac13e9c8c3833573
Divanshu Tandon <divanshutandon6106@gmail.com>
2026-07-20 09:31:14 +0000

    Sync LeetCode submission Runtime - 1 ms (99.94%), Memory - 46.1 MB (99.64%)

--- solutions/0136-single-number/solution.java
+++ solutions/0136-single-number/solution.java
@@ -1,28 +1,10 @@
-// class Solution {
-//     public int singleNumber(int[] nums) {
-//         for (int i = 0; i < nums.length; i++) {
-//             boolean hasDuplicate = false;
-//             for (int j = 0; j < nums.length; j++) {
-//                 if (i != j && nums[i] == nums[j]) {
-//                     hasDuplicate = true;
-//                     break; 
-//                 }
-//             }
-//             if (!hasDuplicate) {
-//                 return nums[i]; 
-//             }
-//         }
-//         return -1;
-//     }
-// }
 class Solution {
     public int singleNumber(int[] nums) {
-        int result = 0;
-        
-        for (int i = 0; i < nums.length; i++) {
-            result ^= nums[i]; // XOR each element with the running result
+        int unique =0;
+        for(int n : nums) {
+            unique ^=n;
         }
+        return unique;
         
-        return result;
     }
 }
